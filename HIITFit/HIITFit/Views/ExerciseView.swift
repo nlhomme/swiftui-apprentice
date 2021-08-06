de5c78b7e00d890e3a12dc167966984069e8c3bf
Nicolas Lhomme <nicolas.lhomme@protonmail.com>
2021-08-06 16:16:08 +0200

    chapter 3: playing a video

--- HIITFit/HIITFit/Views/ExerciseView.swift
+++ HIITFit/HIITFit/Views/ExerciseView.swift
@@ -31,19 +31,29 @@
 /// THE SOFTWARE.
 
 import SwiftUI
+import AVKit
 
 struct ExerciseView: View {
     let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
     let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
     let index: Int
     var body: some View {
-        VStack {
-            HeaderView(exerciseName: exerciseNames[index])
-            Text("Video player")
-            Text("Timer")
-            Text("Start/Done button")
-            Text("Rating")
-            Text("History button")
+        GeometryReader { geometry in
+            VStack {
+                HeaderView(exerciseName: exerciseNames[index])
+                    .padding(.bottom)
+                if let url = Bundle.main.url(forResource: videoNames[index], withExtension: "mp4") {
+                    VideoPlayer(player: AVPlayer(url: url))
+                        .frame(height: geometry.size.height * 0.45)
+                } else {
+                    Text("Couldn't \(videoNames[index]).mp4")
+                        .foregroundColor(.red)
+                }
+                Text("Timer")
+                Text("Start/Done button")
+                Text("Rating")
+                Text("History button")
+            }
         }
     }
 }
